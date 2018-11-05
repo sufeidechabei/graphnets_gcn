9ec2e97cb063c58e349b8e8c814a3bc793dd2de5
sufeidechabei <sufeidechabei@outlook.com>
2018-11-05 14:46:13 +0800

    gcn

--- gcn.py
+++ gcn.py
@@ -12,36 +12,6 @@ import time
 import graph_nets
 
 
-'''
-a = tf.constant([0.5088539, 0.5530578, 0.15916789])
-indice = tf.contrib.framework.argsort(a, -1)
-rank = tf.contrib.framework.argsort(indice, -1)/2
-sess = tf.Session()
-x = tf.stack((a[:-1], a[1:]))[None]
-x_shape = tf.shape(x)
-si = tf.cast(tf.squeeze(a), tf.int32)
-nodes = tf.one_hot(si[:1], 3)
-node_shape = tf.shape(nodes)
-shape = tf.shape(a[:, None])
-input_graphs = []
-input_graphs.append({"nodes":a[:, None]})
-input_graphs = utils_tf.data_dicts_to_graphs_tuple(input_graphs)
-input_graphs = utils_tf.fully_connect_graph_dynamic(input_graphs)
-input_graph = utils_tf.get_graph(input_graphs, 0)
-senders = input_graph.senders
-receivers = input_graph.receivers
-stack_sender_receiver = tf.stack((senders, receivers), axis=1)
-stack_again = tf.shape(stack_sender_receiver[:, :, None])
-print(sess.run([senders, receivers, stack_sender_receiver, stack_again, x_shape]))
-nodes = tf.constant(np.array([[0], [0], [0], [0]]))
-edges = tf.constant(np.array([[0]]))
-graph = input_graph.replace(nodes=nodes, edges=edges)
-receivers = blocks.broadcast_receiver_nodes_to_edges(graph)
-senders = blocks.broadcast_sender_nodes_to_edges(graph)
-print(sess.run([receivers, senders]))
-'''
-
-
 parser = argparse.ArgumentParser(description='GCN')
 parser.add_argument("--gpu", type=int, default=-1,
                     help="gpu")
@@ -78,7 +48,7 @@ def make_linear_model():
         layer_list.append(snt.Linear(num_classes))
         layer_list.append(MultiAdj())
         layer_list.append(ReLu())
-        return snt.Sequential(layer_list)
+    return snt.Sequential(layer_list)
 
 # Define the Adj matrix (served as the support).
 
